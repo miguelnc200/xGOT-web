36a42f7cc60e3595ddd59310a24173081ae3f3eb
miguelnc200 <152646639+miguelnc200@users.noreply.github.com>
2025-07-27 14:08:36 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -16,9 +16,9 @@ def calculate_xsit():
         balon_pos = np.array(data["pos_balon"])           # 3D
         portero = np.array(data["portero"])                # 3D
         end_pos = np.array(data["end_pos"])                # 3D
-        outcome = "Goal"                                   # (o ajustable según lógica)
+        portero_end = np.array(data["porterofin"])
 
-        xgot = compute_xgot_modified(outcome, balon_pos, portero, end_pos)
+        xgot = compute_xgot_modified(balon_pos, portero, end_pos, portero_end)
 
         return jsonify({'xsit': round(float(xgot), 2)})
     except Exception as e:
